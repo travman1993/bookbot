3485d92741a87c89d9eb5b5b33cbe745fbbd40a6
Travis Rodriguez <travisgolembiewski@gmail.com>
2024-08-19 18:47:55 -0400

    walkthrough report done

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    word_count = count_words(text)
+    print(f"The Frankenstein book should contain {word_count} words!")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+main()
+
